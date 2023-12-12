855c31c330dad6c16be691fff3c037c511fd6ee2
Dmitry <dmitry27zh@gmail.com>
2023-12-12 10:14:20 +0500

    Start HangmanDrawing jsx

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,5 +1,46 @@
 const HangmanDrawing = () => {
-  return <div></div>
+  return (
+    <div
+      style={{
+        position: 'relative',
+      }}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          height: '50px',
+          width: '10px',
+          background: 'black',
+          marginLeft: '120px',
+        }}
+      />
+      <div
+        style={{
+          height: '10px',
+          width: '200px',
+          background: 'black',
+          marginLeft: '120px',
+        }}
+      />
+      <div
+        style={{
+          height: '400px',
+          width: '10px',
+          background: 'black',
+          marginLeft: '120px',
+        }}
+      />
+      <div
+        style={{
+          height: '10px',
+          width: '250px',
+          background: 'black',
+        }}
+      />
+    </div>
+  )
 }
 
 export default HangmanDrawing
